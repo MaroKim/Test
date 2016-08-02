1ae8b4746448bc0e70ef93c190756ed0e6d8d18b
yoonwonsang <wonsangyoon@naver.com>
2016-08-02 15:17:39 +0900

    ImageCache 추가

---
+++ coords/ImageCache.h
@@ -0,0 +1,19 @@
+//
+//  ImageCache.h
+//  coords
+//
+//  Created by yoonwonsang on 2016. 7. 30..
+//  Copyright © 2016년 yoonwonsang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface ImageCache : NSObject
+{
+    NSMutableArray *imgMemCache;
+}
+
+
+
+@end
---
+++ coords/ImageCache.m
@@ -0,0 +1,13 @@
+//
+//  ImageCache.m
+//  coords
+//
+//  Created by yoonwonsang on 2016. 7. 30..
+//  Copyright © 2016년 yoonwonsang. All rights reserved.
+//
+
+#import "ImageCache.h"
+
+@implementation ImageCache
+
+@end
